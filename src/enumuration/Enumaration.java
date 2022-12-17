d41326fd04e92b7224e7900725eb6ff47dc5f8a2
Amanbek Aziz Uulu <amanbekazizuulu@gmail.com>
2022-12-17 12:10:44 +0600

    added enum package

---
+++ src/enumuration/Enumaration.java
@@ -0,0 +1,28 @@
+package enumuration;
+
+public class Enumaration {
+	public static void main(String[] args) {
+		Animal animal = Animal.CAT;
+		
+		switch (animal) {
+		case CAT: {
+			System.out.println("It's a cat!");
+			break;
+		}
+		case DOG: {
+			System.out.println("It's a dog!");
+			break;
+		}
+		case TIGER: {
+			System.out.println("It's a tiger!");
+			break;
+		}
+		case FROG: {
+			System.out.println("It's a frog!");
+			break;
+		}
+		default:
+			System.out.println("It is not an animal!");
+		}
+	}
+}
---
+++ src/enumuration/Main.java
@@ -0,0 +1,8 @@
+package enumuration;
+
+public class Main {
+	public static void main(String[] args) {
+		Animal animal = Animal.CAT;
+		
+	}
+}
